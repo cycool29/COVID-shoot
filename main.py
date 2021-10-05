e6d141f5750430b1b769be868adf92c798ccb434
cycool29 <cycool29@gmail.com>
2021-10-05 08:10:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Import the pygame module
 import pygame
 import random
+import math
+
 
 # Initialize pygame module
 pygame.init()
@@ -27,20 +29,28 @@ sanitizer_y = 480
 sanitizer_x_move = 0
 sanitizer_y_move = 0
 
+covid = []
+covid_x = []
+covid_y = []
+covid_y_move = []
+covid_x_move = []
+covid_num = 6
+
+for i in range(covid_num):
+    # Create enemy (COVID)
+    covid.append(pygame.image.load("covid_icon.bmp"))
+
+    # Position
+    # y -= number (up), y += number (down)
+    # x -= number (left), y += number (right)
+    covid_x.append(random.randint(10, 800))
+    covid_y.append(random.randint(50, 150))
+
+    # Movement of sanitizer to be added to sanitizer_x and sanitizer_y
+    covid_x_move.append(3)
+    covid_y_move.append(0.1)
+
 # Create enemy (COVID)
-covid = pygame.image.load("covid_icon.bmp")
-
-# Position
-# y -= number (up), y += number (down)
-# x -= number (left), y += number (right)
-covid_x = random.randint(10, 800)
-covid_y = random.randint(50, 150)
-
-# Movement of sanitizer to be added to sanitizer_x and sanitizer_y
-covid_x_move = 3
-covid_y_move = 0.1
-
-# Create hand enemy (COVID)
 droplets = pygame.image.load("water_drops.bmp")
 
 # Position
@@ -57,6 +67,17 @@ droplets_state = "ready"
 droplets_x_move = 0
 droplets_y_move = 10
 
+# Score
+score_value = 0
+font = pygame.font.Font('Quicksand-SemiBold.ttf', 32)
+
+text_x = 10
+text_y = 10
+
+
+def show_score(x, y):
+    score = font.render("Score: " + str(score_value), True, (0, 0, 0))
+    screen.blit(score, (x, y))
 
 # Define function to show and control sanitizer
 def sanitizer_control(x, y):
@@ -64,8 +85,16 @@ def sanitizer_control(x, y):
 
 
 # Define function to show and control COVID
-def covid_control(x, y):
-    screen.blit(covid, (x, y))
+def covid_control(x, y, img):
+    screen.blit(covid[img], (x, y))
+
+
+def isCollision(enemy_x, enemy_y, bullet_x, bullet_y):
+    distance = math.sqrt((math.pow(enemy_x - bullet_x, 2)) + (math.pow(enemy_y - bullet_y, 2)))
+    if distance < 27:
+        return True
+    else:
+        return False
 
 
 def shoot(x, y):
@@ -114,14 +143,27 @@ while running:
     elif sanitizer_y >= 525:
         sanitizer_y = 525
 
-    covid_x += covid_x_move
-    covid_y += covid_y_move
+    for i in range(covid_num):
+        covid_x[i] += covid_x_move[i]
+        covid_y[i] += covid_y_move[i]
 
-    if covid_x <= -10:
-        covid_x_move = 3
-        covid_y += covid_y_move
-    elif covid_x >= 750:
-        covid_x_move = -3
+        if covid_x[i] <= -10:
+            covid_x_move[i] = 3
+            covid_y[i] += covid_y_move[i]
+        elif covid_x[i] >= 750:
+            covid_x_move[i] = -3
+            covid_y[i] += covid_y_move[i]
+
+        collision = isCollision(covid_x[i], covid_y[i], droplets_x, droplets_y)
+        if collision:
+            droplets_y = 480
+            droplets_state = "ready"
+            score_value += 1
+            print(score_value)
+            covid_x[i] = random.randint(0, 800)
+            covid_y[i] = random.randint(50, 150)
+
+        covid_control(covid_x[i], covid_y[i], i)
 
     if droplets_y <= 0:
         droplets_state = "ready"
@@ -132,5 +174,5 @@ while running:
         droplets_y -= droplets_y_move
 
     sanitizer_control(sanitizer_x, sanitizer_y)
-    covid_control(covid_x, covid_y)
+    show_score(text_x, text_y)
     pygame.display.update()
